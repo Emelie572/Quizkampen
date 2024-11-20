f0824aafa60e8c9c88a5378a30317a7ec08fc5fd
Emelie572 <wahlestedt.emelie@gmail.com>
2024-11-20 13:23:54 +0100

    First commit, grundstrukturen

--- src/ListOfServers.java
+++ src/ListOfServers.java
@@ -1,2 +1,42 @@
-public class ListOfServers {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+
+public class ListOfServers implements Runnable {
+
+    private Socket playerSocket;
+
+
+    public ListOfServers(Socket socket) {
+        this.playerSocket = socket;
+    }
+
+    @Override
+    public void run() {
+
+        try (PrintWriter outStream = new PrintWriter(this.playerSocket.getOutputStream(), true);
+             BufferedReader inStream = new BufferedReader(new InputStreamReader(this.playerSocket.getInputStream()))) {
+
+             String outputLine;
+             String inputLine;
+
+            Protocol quizProtocol = new Protocol();
+            outputLine = quizProtocol.startQuiz();
+            outStream.println(outputLine);
+
+            while ((inputLine = inStream.readLine()) != null) {
+                outputLine = quizProtocol.playerAnswers(inputLine);
+                outStream.println(outputLine);
+
+                if (quizProtocol.quitGame()) {
+                break;
+            }
+        }
+    } catch (IOException e ) {
+            System.out.println("Error med anslutning " + e.getMessage());
+
+        }
+    }
 }
