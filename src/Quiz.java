51c7c39880001610092ec0b7dc40708386196974
Emily Ayou <Emilyayou@outlook.com>
2024-11-20 14:10:59 +0100

    Tja

--- src/Quiz.java
+++ src/Quiz.java
@@ -1,2 +1,56 @@
-public class Quiz {
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class Quiz implements ActionListener {
+
+    String[] frågor = {"Vilken färg är solen?", "Är äpple en frukt eller en grönsak?"};
+
+    String[][] alternativ = {{"Gul", "Rosa"}, {"Grönsak", "Frukt"}};
+
+    char[] svar = {'A', 'B'};
+
+
+    char gissa;
+    char fråga;
+    int index;
+    int korrektSvar;
+    int totaltAntalFrågor = frågor.length;
+    int result;
+
+    JFrame jf = new JFrame();
+    JTextField jt = new JTextField();
+    JTextArea jta = new JTextArea();
+    JButton jbA = new JButton();
+    JButton jbB = new JButton();
+    JLabel jl_svarA = new JLabel();
+    JLabel jl_svarB = new JLabel();
+
+
+    public Quiz() {
+        jf.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        jf.setSize(700, 700);
+        jf.setVisible(true);
+    }
+
+    public void nästaFråga() {
+
+    }
+
+    public void svar() {
+
+    }
+
+    public void resultat() {
+
+    }
+
+    public static void main(String[] args) {
+        Quiz quiz = new Quiz();
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+    }
 }
