38e3b2536ee1099dac35a337cf6aaf670a9de840
ErikoSod <erikos87@hotmail.com>
2024-11-20 04:21:27 +0100

    Added code to ScoreTable class. Upptaded Protocol with better state logic.

--- src/Protocol.java
+++ src/Protocol.java
@@ -1,47 +1,44 @@
 public class Protocol {
 
-    private final int WAITING = 0;
+    private final int SENDINGQUIZ = 0;
     private final int ROUNDSCORE = 1;
-    private final int FINISHED = 2;
-    private int state = WAITING;
+    private int state = SENDINGQUIZ;
 
     private int rounds = 0;
-    private int playerCheckerCount = 0;
-    private Quiz outputQuiz = new Quiz();
+    private int playerStateCount = 0;
+    private Quiz outputQuiz;
     private ScoreTable scoreTable = new ScoreTable();
 
     //TODO set this.rounds from properties.
     public Protocol() {
         this.rounds = 2;
     }
-
-    public synchronized Quiz proccesQuizInput(Quiz inputQuiz) {
-        if (state == WAITING ) {
-            if (playerCheckerCount == 1){
-                playerCheckerCount++;
+//TODO Separat metod för playerStateScore hantering.
+    public synchronized Quiz proccesQuizInput(Quiz inputQuiz)  {
+        if (state == SENDINGQUIZ) {
+            if (playerStateCount == 0) {
+                outputQuiz = new Quiz();
+                playerStateCount++;
+            }else if (playerStateCount == 1){
+                playerStateCount++;
                 state = ROUNDSCORE;
             }
-            playerCheckerCount++;
-
+//TODO Definera dataflöde. Ändra så att setScoreMessage skickas när båda spelare har
+// uppdaterat scoreTable.
         } else if (state == ROUNDSCORE) {
-            String playerName = inputQuiz.playerName();
-            int score = inputQuiz.getScore();
-            scoreTable.updateScoreTable(playerName, score);
-            inputQuiz.setScoreMessage(scoreTable);
-            outputQuiz = inputQuiz;
-
-            if (playerCheckerCount == 1){
-                playerCheckerCount--;
-                state = WAITING;
-                if(rounds == 0){
-                    state = FINISHED;
-                }
-                rounds--;
+            if (playerStateCount == 2) {
+                scoreTable.updateScoreTable(inputQuiz.playerName(),inputQuiz.getScore());
+                playerStateCount--;
+                return null;
+            }else if (playerStateCount == 1) {
+                scoreTable.updateScoreTable(inputQuiz.playerName(),inputQuiz.getScore());
+                inputQuiz.setScoreMessage(scoreTable);
+                inputQuiz.readOnly(true);
+                outputQuiz = inputQuiz;
+                playerStateCount--;
+                state = SENDINGQUIZ;
             }
-            playerCheckerCount--;
-        } else if (state == FINISHED) {
-            outputQuiz = new Quiz();
-            state = WAITING;
+            //TODO lägg till logik för antal rundor.
         }
         return outputQuiz;
     }
