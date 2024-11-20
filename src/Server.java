4290b955a0f35824e7700dc2564d088e500b632d
Emelie572 <wahlestedt.emelie@gmail.com>
2024-11-20 14:14:53 +0100

    First commit, grundstrukturen

--- src/Server.java
+++ src/Server.java
@@ -1,2 +1,31 @@
+
+import java.net.Socket;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Server {
+
+    private List<ListOfServers> players = new ArrayList<>();
+    private ServerListener serverListener;
+
+    public Server(int port) {
+        serverListener = new ServerListener(port);
+    }
+
+    public void start() {
+        serverListener.start(this);
+    }
+
+    public void addPlayer(Socket playerSocket) {
+        ListOfServers listOfServers = new ListOfServers(playerSocket);
+        players.add(listOfServers);
+        new Thread(listOfServers).start();
+    }
+
+    public static void main (String[] args) {
+        Server server = new Server(12345);
+        server.start();
+
+
+    }
 }
