82545433a6b0096b1b8f22b1b90fbcfcf677dc53
Emelie572 <wahlestedt.emelie@gmail.com>
2024-11-21 17:30:26 +0100

    run metod

--- src/MultiPlayer.java
+++ src/MultiPlayer.java
@@ -1,56 +1,28 @@
+import java.io.ObjectOutputStream;
 import java.util.ArrayList;
-import java.io.PrintWriter;
 import java.util.List;
-import java.util.logging.Handler;
+
 
 public class MultiPlayer {
 
-    private final List<PrintWriter> writers = new ArrayList<>();
-    private final List<String> players = new ArrayList<>();
+    private final List<ObjectOutputStream> objectStreams = new ArrayList<>();
     private Protocol protocol;
-    //private  Quiz quiz;
-    //private boolean gameStarted = false;
-    //MultiPlayer multiPlayer = new MultiPlayer();
 
 
-    public synchronized void addPlayer(PrintWriter writer) {
-        writers.add(writer);
+    public synchronized void addPlayers(ObjectOutputStream stream) {
+        objectStreams.add(stream);
         notifyAll();
     }
 
-    public synchronized void waitForTwoPlayers(PrintWriter outstream) throws InterruptedException {
-        if (writers.size() == 1) {
-            outstream.println("Väntar på två spelare");
-
-            while (writers.size() > 2) {
-                wait();
-            }
-
-            for (PrintWriter writer : writers) {
-                writer.println("Två spelare är anslutna! Nu börjar spelet");
-            }
-
-
-            public synchronized void sendQuizPlayerOne (Quiz quiz){
-                if (writers.size() == 2) { //
-                    PrintWriter firstPlayer = writers.get(0);
-                    PrintWriter secondPlayer = writers.get(1);
-
-                    firstPlayer.println(quiz.toString());
-
-                    secondPlayer.println("Spelare ett spelar");
-                }
-
-
-                public synchronized void sendQuizPlayerTwo (Quiz quiz){
-                    if (writers.size() == 2) {
-                        PrintWriter secondPlayer = writers.get(1);
-
-                        secondPlayer.println(quiz.toString());
-                    }
-                }
+    public synchronized void sendProtocalToPlayer(Quiz quiz) {
+        for (ObjectOutputStream stream : objectStreams) {
+            try {
+                stream.writeObject(protocol.proccesQuizInput(quiz));
+                stream.flush();
+            } catch (Exception e) {
+                System.err.println("Fel uppstod med quizzet: " + e.getMessage());
             }
         }
-
     }
 }
+
