877883477c03d818fe4be5c0b5ec6805d147cf7c
ErikoSod <erikos87@hotmail.com>
2024-11-26 23:03:12 +0100

    Created CategorySource with URL.

---
+++ src/main/java/Database/TriviaCategory.java
@@ -0,0 +1,14 @@
+package Database;
+
+public class TriviaCategory{
+    private int id;
+    private String name;
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
--- src/main/java/Quiz.java
+++ src/main/java/Quiz.java
@@ -27,13 +27,13 @@ public Quiz(int category,boolean requestQuiz) {
     this.category = category;
     QuizSourceReader quizSourceReader = new QuizSourceReader(getNumberOfQuestionsProperty(),category);
     questionMaker(quizSourceReader.getQuizSource());
-    System.out.println("Quiz"+quizSourceReader.getQuizSource().results.toString()); //test
+    System.out.println("Quiz"+quizSourceReader.getQuizSource().getResults().toString()); //test
 
     }
 
     private void questionMaker (QuizSource quizSource){
 
-        for(Result result: quizSource.results){
+        for(Result result: quizSource.getResults()){
             List<String> question = new ArrayList<>();
             question.add(result.question);
             question.addAll(shuffleAndSetCorrectAnswers(question,result.incorrect_answers,result.correct_answer));
