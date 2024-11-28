2efaff5294592e888011a83600afba771e241022
Emelie572 <wahlestedt.emelie@gmail.com>
2024-11-28 11:04:29 +0100

    Start gui satt färg och centrering

--- src/main/java/StartGUI.java
+++ src/main/java/StartGUI.java
@@ -9,9 +9,11 @@ public class StartGUI extends JPanel
     JButton button = new JButton("Enter");
     JPanel panel = new JPanel(new GridLayout(3,1));
     JTextField textField = new JTextField(20);
-    JLabel label = new JLabel("Enter Your Name:");
+    JLabel label = new JLabel("Enter Your Name:",SwingConstants.CENTER);
     String name;
     private ActionListener nameListener;
+    private final Color BLUE_COLOR = new Color(30, 70, 150);
+    private final Color WHITE_COLOR = new Color(255, 255, 255);
 
     public String getname(){
         return name;
@@ -19,13 +21,18 @@ public class StartGUI extends JPanel
 
     StartGUI()
     {
+        setBackground(BLUE_COLOR);
+        panel.setBackground(BLUE_COLOR);
+        label.setForeground(WHITE_COLOR);
+
         panel.add(label);
         panel.add(textField);
         panel.add(button);
         add(panel);
-        button.setBorder(new LineBorder(Color.BLUE,1, true));
+        button.setBorder(new LineBorder(WHITE_COLOR,1, true));
         button.setPreferredSize(new Dimension(80, 30));
         label.setFont(new Font("Arial", Font.PLAIN, 15));
+
 
         button.addActionListener(new ActionListener()
         {
@@ -36,6 +43,8 @@ public class StartGUI extends JPanel
                 label.setText("Välkommen " + textField.getText() +"\n"+"Väntar på motståndare..."); //label.setText("Välkommen " + textField.getText());
                 label.setFont((new Font("Arial", Font.PLAIN, 20)));
                 button.setVisible(false);
+                button.setForeground(WHITE_COLOR);
+                button.setBackground(BLUE_COLOR);
                 textField.setVisible(false);
                 getname();
 
@@ -55,6 +64,7 @@ public class StartGUI extends JPanel
         label.setText("Enter Your Name:");
         textField.setVisible(true);
         textField.setFont(getFont().deriveFont(Font.PLAIN));
+        textField.setForeground(WHITE_COLOR);
         button.setVisible(true);
         textField.setText("");
     }
